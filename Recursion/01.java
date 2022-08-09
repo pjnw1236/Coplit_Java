2a381b2187145377f1c8497f0f2a6e1770ac20f1
Park Jun Woo <97827368+pjnw1236@users.noreply.github.com>
2022-08-09 10:32:15 +0900

    Update 01.java

--- Recursion/01.java
+++ Recursion/01.java
@@ -1,8 +1,13 @@
 package com.codestates.coplit; 
 
-public class Solution { 
-  public int sumTo(int num){
-	  if (num == 0) { return 0; }
-	  return num+sumTo(num-1);
-  }
+public class Solution {
+    public int sumTo(int num){
+        if (num == 0) { return 0; }
+        return num+sumTo(num-1);
+    }
 }
+
+
+//        Solution sol = new Solution();
+//        int output = sol.sumTo(10);
+//        System.out.println(output);
