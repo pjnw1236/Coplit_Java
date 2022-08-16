d915a0064398a00a7db8b0befa484342908449a8
Park Jun Woo <97827368+pjnw1236@users.noreply.github.com>
2022-08-16 09:40:58 +0900

    Create 07.java

---
+++ Data_Structure/07.java
@@ -0,0 +1,31 @@
+package com.codestates.coplit;
+import java.util.*;
+
+public class Solution {
+    private int[][] graph;
+
+    public void setGraph(int size) {
+        graph = new int[size][size];
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++) { graph[i][j] = 0; }
+        }
+    }
+  
+    public int[][] getGraph() { return graph; }
+  
+    public void addEdge(int from, int to) {
+        if(from >= graph.length || to >= graph.length) return;
+        graph[from][to] = 1;
+    }
+
+    public boolean hasEdge(int from, int to) {
+        if(from >= graph.length || to >= graph.length) return false;
+        else if(graph[from][to] == 1) return true;
+        else return false;
+    }
+
+    public void removeEdge(int from, int to) {
+        if(from >= graph.length || to >= graph.length) return;
+        graph[from][to] = 0;
+    }
+}
