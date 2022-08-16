8e75e86e3cef82f6b6fcc3df7e4c74110d05fb64
Park Jun Woo <97827368+pjnw1236@users.noreply.github.com>
2022-08-16 09:38:34 +0900

    Update 06.java

--- Data_Structure/06.java
+++ Data_Structure/06.java
@@ -5,41 +5,39 @@ public class Solution {
     private String value;
     private ArrayList<Solution> children;
 
-    public Solution() {	 //전달인자가 없을 경우의 생성자
+    //전달인자가 없을 경우의 생성자
+    public Solution() {	 
         this.value = null;
         this.children = null;
     }
-    public Solution(String data) {	//전달인자가 존재할 경우의 생성자
+    
+    //전달인자가 존재할 경우의 생성자
+    public Solution(String data) {	
         this.value = data;
         this.children = null;
     }
 
-    public void setValue(String data) {
-        this.value = data;
-    }
-
-    public String getValue() {  //현재 노드의 데이터를 반환
-        return value;
-    }
+    // setter와 getter
+    public void setValue(String data) { this.value = data; }
+    public String getValue() { return value; }
+    
+    // node를 tree에 추가
     public void addChildNode(Solution node) {
         if(children == null) children = new ArrayList<>();
         children.add(node);
     }
-    public void removeChildNode(Solution node) {
-        if(children != null) children.remove(node);
-    }
+    
+    // node를 tree에서 삭제
+    public void removeChildNode(Solution node) { if(children != null) children.remove(node); }
 
-    public ArrayList<Solution> getChildrenNode() {
-        return children;
-    }
+    // tree에 존재하는 자손 반환
+    public ArrayList<Solution> getChildrenNode() { return children; }
 
-    public boolean contains(String data) {  //재귀를 사용하여 값을 검색
+    // 재귀를 사용하여 tree에서 value 찾기
+    public boolean contains(String data) {  
         if(value.equals(data)) return true;
-
         if(children != null) {
-            for(int i = 0; i < children.size(); i++) {
-                return children.get(i).contains(data);
-            }
+            for(int i = 0; i < children.size(); i++) { return children.get(i).contains(data); }
         }
         return false;
     }
