51336c0b1810ad5c43f6f0f32eadd00e0a2edaad
Park Jun Woo <97827368+pjnw1236@users.noreply.github.com>
2022-08-16 09:34:15 +0900

    Update 05.java

--- Data_Structure/05.java
+++ Data_Structure/05.java
@@ -1,29 +1,29 @@
-package com.codestates.coplit; 
+package com.codestates.coplit;
 import java.util.*;
 
-public class Solution { 
-	public int queuePrinter(int bufferSize, int capacities, int[] documents) {
-    
-    Queue<Integer> q = new LinkedList<>();
-    for (int doc : documents) { q.add(doc); }
-    int[] arr = new int[bufferSize];
+public class Solution {
+    public int queuePrinter(int bufferSize, int capacities, int[] documents) {
 
-    int time = 1;
-    arr[0] = q.poll();
+        Queue<Integer> q = new LinkedList<>();
+        for (int doc : documents) { q.add(doc); }
+        int[] arr = new int[bufferSize];
 
-    while (!q.isEmpty()) {
-      time += 1;
-      for (int i=bufferSize-2; i>=0; i--) {
-      arr[i+1] = arr[i];
-      }
-            
-      arr[0] = 0;
-      if ((q.peek() + Arrays.stream(arr).sum()) <= capacities) {
+        int time = 1;
         arr[0] = q.poll();
-        }
-    }
-    
-    return time + bufferSize;
 
-	} 
+        while (!q.isEmpty()) {
+            time += 1;
+            for (int i=bufferSize-2; i>=0; i--) {
+                arr[i+1] = arr[i];
+            }
+
+            arr[0] = 0;
+            if ((q.peek() + Arrays.stream(arr).sum()) <= capacities) {
+                arr[0] = q.poll();
+            }
+        }
+
+        return time + bufferSize;
+
+    }
 }
